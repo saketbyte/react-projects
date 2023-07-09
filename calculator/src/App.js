bcbea524fc995d63cfebd5d2ba8d313dc12235ee
saketbyte <samriddhsingh00@gmail.com>
2023-07-10 02:16:25 +0530

    Commented all the code for personal understanding

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -54,11 +54,15 @@ function reducer(state, { type, payload }) {
     //  To clear the screen return an empty object for a situation just like at the beginning.
     case ACTIONS.CLEAR:
       return {};
-    //
+    // to calculate based on +,=,/ and x and handling some edge cases.
     case ACTIONS.CHOOSE_OPERATION:
+      // If there is nothing to calculate
       if (state.currentOperand == null && state.previousOperand == null) return state;
-
+      //  The second operand has not been entered, and the user pressed an operator key, but now wants to go for a different operand key, so update the operation: paylod.operation value and move ahead.
       if (state.currentOperand == null) return { ...state, operation: payload.operation };
+
+      //  Real beauty of the calculator:
+      // This if condition sets the previous operand and makes space for new operand to which this should be added. 20 + was given but it was like - prev = null, curr = 20 and op = +, it transforms it to the new state as prev = 20, curr = null and op = +
 
       if (state.previousOperand == null) {
         return {
@@ -68,7 +72,7 @@ function reducer(state, { type, payload }) {
           currentOperand: null,
         };
       }
-
+      //  Send the previousOperand as newly calculated result and operation as the newly selected operand if any otherwise null.
       return {
         ...state,
         previousOperand: evaluate(state),
@@ -80,29 +84,33 @@ function reducer(state, { type, payload }) {
         return {
           ...state,
           overwrite: false,
-          currentOperand: null,
+          currentOperand: null, // Make the current operand null, ie nothing to display
         };
       }
-      if (state.currentOperand == null) return state;
+      if (state.currentOperand == null) return state; // return as it is if there is nothing to delete
       if (state.currentOperand.length === 1) {
-        return { ...state, currentOperand: null };
+        return { ...state, currentOperand: null }; // edge case: if only one digit is typed then remove that and return null
       }
 
       return {
         ...state,
-        currentOperand: state.currentOperand.slice(0, -1),
+        currentOperand: state.currentOperand.slice(0, -1), // return everything but the last entered character
       };
 
     case ACTIONS.EVALUATE:
+      //  We can not evaluate if we do not have all 3 information bits.
       if (state.operation == null || state.currentOperand == null || state.previousOperand == null) {
         return state;
       }
 
       return {
         ...state,
-        overwrite: true, // overwrite is set to true here, to overwrite the result displayed after
-        // which the user would like the new data to be displayed.
-        //  Updated at -> EVALUATE, DELETE_DIGIT AND ADD_DIGIT
+        overwrite: true,
+
+        /* overwrite is set to true here, to overwrite the result displayed after  which the user     would like the new data to be displayed.
+          Updated at -> EVALUATE, DELETE_DIGIT AND ADD_DIGIT
+          */
+        // When evaluated
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
@@ -112,7 +120,8 @@ function reducer(state, { type, payload }) {
   }
 }
 
-/*
+function evaluate({ currentOperand, previousOperand, operation }) {
+  /*
 JavaScript does not enforce strict parameter matching, which allows you to pass any number of arguments to a function. This flexibility can be useful in certain cases, such as when you want to write functions that can handle a variable number of arguments or when you want to ignore additional arguments intentionally. However, it's important to design your functions with clear expectations and document the expected arguments to avoid confusion and potential bugs.
 
 function addNumbers(a, b) {
@@ -126,14 +135,17 @@ console.log(addNumbers(2, 3, 4));  // Output: a: 2, b: 3
 
 
 */
-function evaluate({ currentOperand, previousOperand, operation }) {
+
+  // converting the strings to floating point literals.
   const prev = parseFloat(previousOperand);
   const curr = parseFloat(currentOperand);
 
+  // if any of the operand is not defined return empty string.
   if (isNaN(prev) || isNaN(curr)) return "";
 
   let computation = "";
 
+  // Perform the operation and return the result based on the operation value.
   switch (operation) {
     case "+":
       computation = prev + curr;
@@ -156,8 +168,10 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation;
 }
 
+// Google it when needed.
 const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", { maximumFractionDigits: 0 });
 
+// Function to split based on the decimal and format only specific part and rejoin and return the formatted part.
 function formatOperand(operand) {
   if (operand == null) return;
   const [integer, decimal] = operand.toString().split(".");
@@ -166,24 +180,37 @@ function formatOperand(operand) {
 }
 
 function App() {
+  // Using the reducer hook
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
 
   // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
+  // JSX starts
   return (
     <div className="calculator-grid">
       <div className="output">
         {/* previous-operand will contain the previous number and calculation sign. current-operand will contain the current operand. */}
+
+        {/* Displaying the formatted operand */}
         <div className="previous-operand">
           {formatOperand(previousOperand)} {operation}
         </div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
 
+      {/* Associating the dispatch function of useReducer hook instead of so many onClickHandler functions we can use the type: attribute in the useReducer to call the same reduce function which will have conditional reducing based on the value that we associate it with in type: attribute.  */}
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
         AC
       </button>
       <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       {/* <DigitButton digit = "÷" dispatch = {dispatch}/> */}
+
+      {/* So one interesting thing that you would see here is:
+      
+      The components have a prop called dispatch which will be used in onClick function of the JSX that they contain. This dispatch prop will contain the dispatch function of the reducer hook itself. Then in the component file, we will specify the type: attribute of the call.
+
+      While the usual buttons being specified here are simple buttons which have the dispatch function mentioned directly with type: attribute.   
+      
+       */}
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
