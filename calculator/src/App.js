e78d380be6bd2f455a7e0ee16903e66ba7ad8bb8
saketbyte <samriddhsingh00@gmail.com>
2023-07-09 23:59:09 +0530

    idek which repo shit

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import DigitButton from "./DigitButton";
 import OperationButton from "./OperationButton";
 
+// Standard way of declaring certain titles.
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CLEAR: "clear",
@@ -11,8 +12,30 @@ export const ACTIONS = {
   CHOOSE_OPERATION: "choose-operation",
 };
 
+// Let me explain the useReducer hook:
+// it TAKES two arguments --> A reducer function as defined below and the initial value which is also the state definition.
+// it RETURNS two things --> current state and a dispatch function.
+// So we have 4 things to talk about - reducer and dispatch function, current state and it's definition.
+
+// reducer --> It is a function which we are supposed to provide to this hook for making updates to the state and provide
+// it as a single function to the useReducer hook. The reducer function in this hook must be defined in a way such that it
+// receives two JS objects as arguments : current state and [ACTION_TYPE and some other information if need be but as single JS object]. Here it is state and {type, payload} object which contains the type and some other information. It returns in the end.
+
+// dispatch --> It first checks if the state is a simple JS object and then it checks if it is defined or undefined.
+// Once CONFIRM IT CALLS THE REDUCER FUNCTION WITH CURRENT STATE AND ACTION TYPE/MORE INFO AND IF A STATE CHANGE IS DETECTED BASED ON THE NEW STATE RETURNED BY THE REDUCER FUNCTION DISPATCH FUNCTION RENDERS THE DOM ELEMENT AGAIN.
+// A lot of this is done in the backend which we do not get to know, so it is abstract.
+
+// Current state is the state being sent to reducer function through dispatch function.
+// Definition of current state: the number of key-value pairs that you need to use in the program for the information to be managed by useReducer state hook is defined when we set the initial value, it can always be updated ofcourse, but for structure we do so there or we pass an empty JS object.
+
+// useReducer is very similar to useState, but it lets you move the state update logic from event handlers into a single function outside of your component. Read more about choosing between useState and useReducer.
+
 function reducer(state, { type, payload }) {
+  // Defining the reducer function for useReducer hook.
+
   switch (type) {
+    // Based on the type received by dispatch function, we can go for the different cases.
+    // to render the digits being entered in the input div of calculator, ADD_DIGIT
     case ACTIONS.ADD_DIGIT:
       if (state.overwrite) {
         return {
@@ -28,9 +51,10 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+    //  To clear the screen return an empty object for a situation just like at the beginning.
     case ACTIONS.CLEAR:
       return {};
-
+    //
     case ACTIONS.CHOOSE_OPERATION:
       if (state.currentOperand == null && state.previousOperand == null) return state;
 
@@ -76,7 +100,9 @@ function reducer(state, { type, payload }) {
 
       return {
         ...state,
-        overwrite: true,
+        overwrite: true, // overwrite is set to true here, to overwrite the result displayed after
+        // which the user would like the new data to be displayed.
+        //  Updated at -> EVALUATE, DELETE_DIGIT AND ADD_DIGIT
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
@@ -86,6 +112,20 @@ function reducer(state, { type, payload }) {
   }
 }
 
+/*
+JavaScript does not enforce strict parameter matching, which allows you to pass any number of arguments to a function. This flexibility can be useful in certain cases, such as when you want to write functions that can handle a variable number of arguments or when you want to ignore additional arguments intentionally. However, it's important to design your functions with clear expectations and document the expected arguments to avoid confusion and potential bugs.
+
+function addNumbers(a, b) {
+  console.log("a:", a);
+  console.log("b:", b);
+  return a + b;
+}
+
+console.log(addNumbers(2, 3));  // Output: a: 2, b: 3
+console.log(addNumbers(2, 3, 4));  // Output: a: 2, b: 3
+
+
+*/
 function evaluate({ currentOperand, previousOperand, operation }) {
   const prev = parseFloat(previousOperand);
   const curr = parseFloat(currentOperand);
